3e7ed521ee459405fa545aa8eab906cd3a0932e2
JingYuan <jingyuan0926@gmail.com>
2025-06-21 13:04:54 +0800

    Fix header to TeaTime

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import { useEffect } from "react";
 import {HeroUIProvider} from "@heroui/react";
+import Header from "@/components/Header";
 
 
 // Stagewise toolbar integration
@@ -50,6 +51,7 @@ export default function App({ Component, pageProps }) {
 
   return (
     <HeroUIProvider>
+      <Header />
       <Component {...pageProps} />
     </HeroUIProvider>
   );
